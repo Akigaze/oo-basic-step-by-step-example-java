4e58b1b1f6a3318f5d8aae27ce0bcf5ccaa90ac6
quinnHuang <quinn.huang@oocl.com>
2018-07-10 15:00:39 +0800

    [Quinn] complete practice 7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,33 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(){}
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getDisplayName(){
+        return String.format("Class %d",this.number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Klass){
+            final Klass klz=(Klass)obj;
+            if (this.number==klz.number){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,27 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student() {}
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduceStr=null;
+        introduceStr=String.format("%s I am a Student. I am at Class %d.",super.introduce(),klass.getNumber());
+        return introduceStr;
+    }
 }
