c9bc04b8d59df8eb4e00c834854a1a61514de8b7
quinnHuang <quinn.huang@oocl.com>
2018-07-10 14:17:37 +0800

    [Quinn] complete practice 4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,15 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker() {
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Worker. I have a job.",super.basicIntroduce());
+    }
 }
