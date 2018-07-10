ceceb1b1887232e7bbf8d46dbd57714f2da9f9e0
quinnHuang <quinn.huang@oocl.com>
2018-07-10 14:11:47 +0800

    [Quinn] complete practice 3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,27 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student() {}
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduceStr=null;
+        introduceStr=String.format("I am a Student. I am at Class %d.",klass);
+        return introduceStr;
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,15 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker() {
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
